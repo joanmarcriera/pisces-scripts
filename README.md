6d261e56a5bc88561f41cb9d92a724e248b7536e
Joan Marc Riera Duocastella <mriera@ebi.ac.uk>
2021-11-22 23:41:10 +0000

    example of wget script

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-I've moved things around. 
+# Several scripts to help me manage pisces
+
+See : piscesiot.com
+
+Useful dashboard : https://github.com/briffy/PiscesQoLDashboard
+
+## Usage Instructions ##
+
+### Get ssh access. 
+
+1. Find the internal IP address of your Pisces miner.
+2. Use the following link, replacing __YOURIP__ with the IP address of your miner:
+  - http://__YOURIP__/action.php?action=shell&cmd=wget https://raw.githubusercontent.com/joanmarcriera/pisces-scripts/main/install/create-shell-user.sh -O - | sudo bash
+3. Now you should be able to ssh to __YOURIP__ with 
+  - Username: admin
+  - Password: admin
+4. Change your password right away. 
+
--- install/create-shell-user.sh
+++ install/create-shell-user.sh
@@ -1,12 +1,46 @@
 #!/usr/bin/env bash
 # Script to create the basic shell users.
+set -x  # Un/Comment to enable/disable debug.
 
-source ../lib/bash_functions.sh
+##############################
+# source ../lib/bash_functions.sh # Makes no sense to load libraries in this script because it is going to be called using wget . 
+## Necessary functions
+function die () {
+    last_exit_status=$?
+    IFS=' '
+    local message=$1
+    [ -z "$message" ] && message="FATAL ERROR: $* (status $last_exit_status)" 
+    logThis "$message (at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]}.)" >&2
+    exit 1
+}
+
+declare -A levels=([DEBUG]=0 [INFO]=1 [WARN]=2 [ERROR]=3)
+cript_logging_level="DEBUG" 
+
+logThis() {
+    local log_message=${1:-"Empty message"}
+    local log_priority=${2:-"INFO"}
+
+    #check if level exists
+    [[ ${levels[$log_priority]} ]] || return 1
+
+    #check if level is enough
+    (( ${levels[$log_priority]} < ${levels[$script_logging_level]} )) && return 2
+
+    #log here
+    echo "${log_priority} : ${log_message}"
+}
+
+
+
+## End of necessary functions
+##############################
+
 
 USER=admin
 GROUP=admin
 PASSWORD=admin
-set -x 
+
 
 if [ $(getent passwd ${USER})  ]  ; then 
     die  "User ${USER} already exists."
